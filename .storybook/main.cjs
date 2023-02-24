75348dd5a7e8ff8de18185af71ae586c00e65227
Karina Grishaeva <revuwem@gmail.com>
2023-02-24 17:36:38 +0400

    misc: set up tailwindcss with storybook integration

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,4 +15,18 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/i,
+      use: [
+        {
+          loader: "postcss-loader",
+          options: { implementation: require.resolve("postcss") },
+        },
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
+    // Return the altered config
+    return config;
+  },
 };
