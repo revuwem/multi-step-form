0c96306a403427d454f60b354bd47ac7aac583f8
Karina Grishaeva <revuwem@gmail.com>
2023-02-24 17:38:11 +0400

    feat: extend tailwindcss color scheme with custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "marine-blue": "var(--color-marine-blue)",
+        "carbon-blue": "var(--color-carbon-blue)",
+        "purplish-blue": "var(--color-purplish-blue)",
+        "pastel-blue": "var(--color-pastel-blue)",
+        "light-blue": "var(--color-light-blue)",
+        "strawberry-red": "var(--color-strawberry-red)",
+        "cool-gray": "var(--color-cool-gray)",
+        "light-gray": "var(--color-light-gray)",
+        magnolia: "var(--color-magnolia)",
+        alabaster: "var(--color-alabaster)",
+        white: "var(--color-white)",
+      },
+    },
   },
   plugins: [],
 };
