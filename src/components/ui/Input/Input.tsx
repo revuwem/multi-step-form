09de1d3d80b79359a6dac841d479d419cc09ecfa
Karina Grishaeva <revuwem@gmail.com>
2023-03-03 22:24:55 +0400

    feat(components): add Input width style

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -3,7 +3,7 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 const InputStyles = {
-  base: "text-sm text-marine-blue font-medium py-2.5 px-3.5 border border-light-gray rounded-md",
+  base: "w-full text-sm text-marine-blue font-medium py-2.5 px-3.5 border border-light-gray rounded-md",
   focusVisible: "focus-visible:outline-none focus-visible:border-purplish-blue",
   error: "border-strawberry-red",
 };
