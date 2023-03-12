f961be114a02b93bb0ae4eb45a4ef335e8e58663
Karina Grishaeva <revuwem@gmail.com>
2023-03-12 21:46:41 +0400

    feat(pages): add 'Success' route to router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Addons from "./Addons/Addons";
 import Info from "./Info/Info";
 import Plan from "./Plan/Plan";
+import Success from "./Success/Success";
 import Summary from "./Summary/Summary";
 
 const router = createBrowserRouter([
@@ -29,6 +30,13 @@ const router = createBrowserRouter([
   {
     path: "/summary",
     element: <Summary />,
+    errorElement: (
+      <div>Sorry, we couldn't find the page you are looking for</div>
+    ),
+  },
+  {
+    path: "/success",
+    element: <Success />,
     errorElement: (
       <div>Sorry, we couldn't find the page you are looking for</div>
     ),
