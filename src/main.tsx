656a095574b23574f54e7ee4d5f6e2ffeffdc13b
Karina Grishaeva <revuwem@gmail.com>
2023-03-17 03:34:33 +0400

    feat: add SubscriptionContextProvider to app

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./pages";
 import "./index.css";
+import { SubscriptionContextProvider } from "./shared/context/Subscription/SubscriptionContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <SubscriptionContextProvider>
+      <RouterProvider router={router} />
+    </SubscriptionContextProvider>
   </React.StrictMode>
 );
