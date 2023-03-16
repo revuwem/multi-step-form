b4807ea29562166ebaf36dd13f2e70c79c1fe50d
Karina Grishaeva <revuwem@gmail.com>
2023-03-17 00:37:17 +0400

    feat(shared/context): create default value for Subscription context

--- src/shared/context/SubscriptionContext.tsx
+++ src/shared/context/SubscriptionContext.tsx
@@ -1,18 +1,28 @@
 import React, { createContext, useContext, useState } from "react";
 
 // Default value
-const subscriptionDefaultValue: ISubscription = {} as ISubscription;
+const subscriptionDefaultValue = {
+  state: {
+    info: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+    plan: null,
+    addons: null,
+    period: "month",
+  } as ISubscription,
+  setState: (state: ISubscription) => {},
+};
 
 // Context
-export const SubscriptionContext = createContext<ISubscription>(
-  subscriptionDefaultValue
-);
+export const SubscriptionContext = createContext(subscriptionDefaultValue);
 
 // Context Provider
 export const SubscriptionContextProvider: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
-  const [state, setState] = useState<ISubscription>(subscriptionDefaultValue);
+  const [state, setState] = useState(subscriptionDefaultValue.state);
 
   return (
     <SubscriptionContext.Provider value={state}>
