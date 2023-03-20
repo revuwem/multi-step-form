534ef3ce160eafe2a7553becaebb94c7d9926963
Karina Grishaeva <revuwem@gmail.com>
2023-03-20 23:18:36 +0400

    refactor(shared/context): remove context check from useSubscriptionState

--- src/shared/context/Subscription/SubscriptionContext.tsx
+++ src/shared/context/Subscription/SubscriptionContext.tsx
@@ -35,11 +35,5 @@ export const SubscriptionContextProvider: React.FC<React.PropsWithChildren> = ({
 export const useSubscriptionState = () => {
   const context = useContext(SubscriptionContext);
 
-  if (!context) {
-    throw new Error(
-      "useSubscriptionState must be used within the SubscriptionProvider"
-    );
-  }
-
   return context;
 };
