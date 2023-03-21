3aba348f2c2a1f71c34456ecfc12c882dd0f27f1
Karina Grishaeva <revuwem@gmail.com>
2023-03-21 23:55:00 +0400

    feat(ui/widget)!: add react-hook-form ref props

--- src/widgets/FormControl/FormControl.tsx
+++ src/widgets/FormControl/FormControl.tsx
@@ -1,21 +1,26 @@
+import { Path, UseFormRegister } from "react-hook-form";
+
 import ErrorLabel from "../../ui/ErrorLabel/ErrorLabel";
 import Input from "../../ui/Input/Input";
 import Label from "../../ui/Label/Label";
 
 interface FormControl extends React.InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  displayedLabel: string;
   error?: boolean;
+  label: Path<ISubscription>;
+  register: UseFormRegister<ISubscription>;
+  required: boolean;
 }
 
 const FormControl: React.FC<FormControl> = ({
-  label,
+  displayedLabel,
   error = false,
   ...props
 }) => {
   return (
     <div className="mb-4">
       <div className="flex justify-between items-end mb-2">
-        <Label htmlFor={props.id}>{label}</Label>
+        <Label htmlFor={props.id}>{displayedLabel}</Label>
         {error && <ErrorLabel>This field is required</ErrorLabel>}
       </div>
       <Input error={error} {...props} />
