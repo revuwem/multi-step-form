131924354e12bc9c494bc62490e14b702427d893
Karina Grishaeva <revuwem@gmail.com>
2023-03-23 01:04:00 +0400

    feat(features/PlanRadioButton): change internal state to use global form state

--- src/features/PlanRadioButton/PlanRadioButton.tsx
+++ src/features/PlanRadioButton/PlanRadioButton.tsx
@@ -1,39 +1,48 @@
-interface PlanRadioButtonProps {
+import { UseControllerProps, useController } from "react-hook-form";
+
+interface PlanRadioButtonProps
+  extends UseControllerProps<ISubscription, "plan">,
+    Omit<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      keyof UseControllerProps<ISubscription, "plan">
+    > {
   id: string;
-  name: string;
-  checked: boolean;
   content: {
     title: string;
     label: string;
     note?: string;
     iconSrc: string;
   };
-  onChange: () => void;
 }
 
 const PlanRadioButton: React.FC<PlanRadioButtonProps> = ({
   content,
+  checked,
   ...props
 }) => {
+  const {
+    field: { value: plan, onChange },
+  } = useController(props);
+
   const labelClassName = [
     "p-3 flex flex-col gap-10 border rounded hover:border-purplish-blue hover:cursor-pointer",
-    props.checked ? "border-purplish-blue" : "border-light-gray",
-    props.checked ? "bg-alabaster" : "bg-white",
+    plan === props.value ? "border-purplish-blue" : "border-light-gray",
+    plan === props.value ? "bg-alabaster" : "bg-white",
   ].join(" ");
-
-  const { title, label, note, iconSrc } = content;
 
   return (
     <label className={labelClassName}>
       <div className="">
-        <img src={iconSrc} alt="" />
+        <img src={content.iconSrc} alt="" />
       </div>
       <div className="flex flex-col gap-1">
-        <p className="text-marine-blue font-medium">{title}</p>
-        <p className="text-sm text-cool-gray">{label}</p>
-        {note && <p className="text-sm text-marine-blue">{note}</p>}
+        <p className="text-marine-blue font-medium">{content.title}</p>
+        <p className="text-sm text-cool-gray">{content.label}</p>
+        {content.note && (
+          <p className="text-sm text-marine-blue">{content.note}</p>
+        )}
       </div>
-      <input type="radio" className="hidden" {...props} />
+      <input type="radio" className="hidden" {...props} onChange={onChange} />
     </label>
   );
 };
