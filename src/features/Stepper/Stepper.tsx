7ddb93db58da79ee745ac01389cdf617d361a770
Karina Grishaeva <revuwem@gmail.com>
2023-04-04 23:57:00 +0400

    fix(features/Stepper): add key prop to Stepper items

--- src/features/Stepper/Stepper.tsx
+++ src/features/Stepper/Stepper.tsx
@@ -12,7 +12,7 @@ const Stepper: React.FC<StepperProps> = ({ steps }) => {
     <aside className="p-5 rounded-xl bg-no-repeat bg-center bg-cover bg-[url('/assets/images/bg-sidebar-mobile.svg')] md:bg-[url('/assets/images/bg-sidebar-desktop.svg')]">
       <ol className="list-none flex gap-5 justify-center md:flex-col md:gap-7">
         {steps.map((step) => (
-          <Step number={step.number} label={step.label} />
+          <Step key={step.label} number={step.number} label={step.label} />
         ))}
       </ol>
     </aside>
