6090733cde1d4cfa1884ec52a45a11e6cfa5860d
Karina Grishaeva <revuwem@gmail.com>
2023-04-05 20:46:06 +0400

    fix: assets paths

--- src/pages/Success/Success.tsx
+++ src/pages/Success/Success.tsx
@@ -10,7 +10,7 @@ const Summary: React.FC<{}> = () => {
           message="Thanks for confirming your subscription! We hope you have
                 fun using our platform. If you ever need support, please feel
                 free to email us at support@loremgaming.com."
-          iconSrc="/assets/images/icon-thank-you.svg"
+          iconSrc="assets/images/icon-thank-you.svg"
         />
       </div>
     </Layout>
