6e4ca8bb39c3473618ea4741e0c11fccb3b0b0e9
Karina Grishaeva <revuwem@gmail.com>
2023-04-05 20:45:39 +0400

    feat(pages): add router basename

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,42 +5,45 @@ import Plan from "./Plan/Plan";
 import Success from "./Success/Success";
 import Summary from "./Summary/Summary";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Info />,
-    errorElement: (
-      <div>Sorry, we couldn't find the page you are looking for</div>
-    ),
-  },
-  {
-    path: "/plan",
-    element: <Plan />,
-    errorElement: (
-      <div>Sorry, we couldn't find the page you are looking for</div>
-    ),
-  },
-  {
-    path: "/addons",
-    element: <Addons />,
-    errorElement: (
-      <div>Sorry, we couldn't find the page you are looking for</div>
-    ),
-  },
-  {
-    path: "/summary",
-    element: <Summary />,
-    errorElement: (
-      <div>Sorry, we couldn't find the page you are looking for</div>
-    ),
-  },
-  {
-    path: "/success",
-    element: <Success />,
-    errorElement: (
-      <div>Sorry, we couldn't find the page you are looking for</div>
-    ),
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Info />,
+      errorElement: (
+        <div>Sorry, we couldn't find the page you are looking for</div>
+      ),
+    },
+    {
+      path: "/plan",
+      element: <Plan />,
+      errorElement: (
+        <div>Sorry, we couldn't find the page you are looking for</div>
+      ),
+    },
+    {
+      path: "/addons",
+      element: <Addons />,
+      errorElement: (
+        <div>Sorry, we couldn't find the page you are looking for</div>
+      ),
+    },
+    {
+      path: "/summary",
+      element: <Summary />,
+      errorElement: (
+        <div>Sorry, we couldn't find the page you are looking for</div>
+      ),
+    },
+    {
+      path: "/success",
+      element: <Success />,
+      errorElement: (
+        <div>Sorry, we couldn't find the page you are looking for</div>
+      ),
+    },
+  ],
+  { basename: "/multi-step-form/" }
+);
 
 export default router;
